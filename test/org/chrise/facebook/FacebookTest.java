47dfeca570fc2406e265e09e70cae27aadcbb26e
Ceva24 <christian@ceva24.co.uk>
2013-06-22 20:46:14 +0100

    Refactored FacebookTest.java to correct package

--- test/org/chrise/facebook/FacebookTest.java
+++ test/org/chrise/facebook/FacebookTest.java
@@ -1,15 +1,14 @@
-package org.chrise.facebook.model;
+package org.chrise.facebook;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import org.chrise.facebook.Facebook;
+import org.chrise.facebook.model.FacebookObject;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-
 
 public class FacebookTest
 {
